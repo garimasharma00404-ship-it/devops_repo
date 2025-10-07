228c0d09b9ed11bed66f3add3a69e4d2810c7af3
garimasharma00404-ship-it <garimasharma00404@gmail.com>
2025-10-07 18:05:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,5 +6,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(8000, () => {
-  console.log("Server running at http://localhost:8000");
+  console.log("Server  running at http://localhost:8000");
 });
